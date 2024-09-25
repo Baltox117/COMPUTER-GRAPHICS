40f6377b186082bd90cfadbdb842b83f1d23000f
Baltazar David Vargas Torres <baltazarvargas117@gmail.com>
2024-09-24 21:18:14 -0600

    change-name

--- sld2-basic/cmake-build-debug/cmake_install.cmake
+++ sld2-basic/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/CLionProjects/sld2-basic
+# Install script for directory: C:/COMPUTER-GRAPHICS/sld2-basic
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/CLionProjects/sld2-basic/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/COMPUTER-GRAPHICS/sld2-basic/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
